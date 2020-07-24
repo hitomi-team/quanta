b4376c059739a2c8a22b3283c11a56a8fcd29afd
sokonashi <seizou.genkei@protonmail.com>
2020-07-23 17:29:15 -0700

    Implement D3D and OGL dependencies

--- src/layer1/graph/node.cpp
+++ src/layer1/graph/node.cpp
@@ -1,4 +1,4 @@
-#include "Node.h"
+#include "node.h"
 
 namespace Graph {
 
---
+++ src/layer1/renderer/subsystems/d3d11/d3d11required.h
@@ -0,0 +1,10 @@
+#ifndef RENDERER_D3D11REQUIRED_H
+#define RENDERER_D3D11REQUIRED_H
+
+#define NOMINMAX
+
+#include <DirectX/D3D11.h>
+#include <DirectX/DXGI.h>
+#include <SDL2/SDL.h>
+
+#endif
