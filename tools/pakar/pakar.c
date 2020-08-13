c2a6bb46dc1db881f2c8583d647fce9343c4a87b
koukuno <kuze.genkei@protonmail.com>
2020-08-12 17:09:57 -0700

    tools/pakar: refactor and remove superfluous code

--- tools/pakar/pakar.c
+++ tools/pakar/pakar.c
@@ -181,38 +181,32 @@ enum {
 	FILE_INFO_REG
 };
 
-struct file_info {
-	char *path;
-	uint64_t modified_time;
-	uint64_t size;
-	int type;
-
-	struct file_info *next;
-};
-
 struct file_entry {
 	struct pakar_index_entry entry;
-	char *path;
+	char *actual_path;
 	struct file_entry *next;
 };
 
-static struct file_info *fi_head = NULL;
+static const char *greater_path = NULL;
+static size_t greater_path_len = 0;
+
+static struct file_entry *fi_head = NULL;
 static uint64_t fi_total_size = 0;
 static uint32_t num_fi = 0;
 
 #define ARRAY_SIZE(x) (sizeof(x) / sizeof(x[0]))
 
 static const char *blacklisted_exts[] = {
-	".zip",
-	".rar",
-	".exe",
-	".dll",
-	".bat",
-	".cmd",
-	".tar",
-	".pak",
-	".com",
-	".7z",
+	"zip",
+	"rar",
+	"exe",
+	"dll",
+	"bat",
+	"cmd",
+	"tar",
+	"pak",
+	"com",
+	"7z",
 };
 
 static const size_t num_blacklisted_exts = ARRAY_SIZE(blacklisted_exts);
@@ -229,9 +223,11 @@ const char *get_filename_ext(const char *filename)
 
 void file_info_add(const char *path, uint64_t modified_time, uint64_t size, int type)
 {
-	struct file_info *fi;
+	struct file_entry *fi;
 	const char *ext;
-	size_t i;
+	size_t i, a;
+
+	a = strlen(path);
 
 	if (type == FILE_INFO_DIR)
 		goto create_entry;
@@ -245,13 +241,25 @@ void file_info_add(const char *path, uint64_t modified_time, uint64_t size, int 
 	}
 
 create_entry:
-	fi = malloc(sizeof(struct file_info));
+	fi = malloc(sizeof(struct file_entry));
 
-	fi->type = type;
-	fi->modified_time = modified_time;
-	fi->size = size;
-	fi->path = malloc(strlen(path) + 1);
-	strcpy(fi->path, path);
+	if (type == FILE_INFO_DIR)
+		fi->entry.crc32 = 0xDEADBEEF;
+	else
+		fi->entry.crc32 = 0xFFFFFFFF;
+
+	fi->entry.modified_time = modified_time;
+	fi->entry.size = size;
+	fi->entry.type = type;
+
+	memset(&fi->entry.path, 0, sizeof(fi->entry.path));
+	if (strcmp(path, greater_path) == 0)
+		fi->entry.path[0] = '/';
+	else
+		memcpy(fi->entry.path, path + greater_path_len, a - greater_path_len);
+
+	fi->actual_path = malloc(strlen(path) + 1);
+	strcpy(fi->actual_path, path);
 
 	fi->next = fi_head;
 	fi_head = fi;
@@ -263,11 +271,11 @@ create_entry:
 
 void file_info_cleanup()
 {
-	struct file_info *fi;
+	struct file_entry *fi;
 
 	while (fi_head != NULL) {
 		fi = fi_head->next;
-		free(fi_head->path);
+		free(fi_head->actual_path);
 		free(fi_head);
 		fi_head = fi;
 	}
@@ -290,7 +298,7 @@ int nftw_fn(const char *path, const struct stat *info, const int type_flag, stru
 	type = type_flag == FTW_F ? FILE_INFO_REG : FILE_INFO_DIR;
 	conv_timespec_to_u64_ms(&info->st_mtim, &time);
 
-	file_info_add(path, time, info->st_size, type);
+	file_info_add(path, time, type == FILE_INFO_DIR ? 0 : info->st_size, type);
 
 	return 0;
 }
@@ -351,7 +359,7 @@ int dir_tree_walk_for_files(const char *dir_path)
 		strcpy(tmp_path, dir_path);
 		strcat(tmp_path, "/");
 		strcat(tmp_path, utf8_path);
-		file_info_add(tmp_path, time, size, type);
+		file_info_add(tmp_path, time, type == FILE_INFO_DIR ? 0 : size, type);
 
 		if (find_data.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY)
 			dir_tree_walk_for_files(tmp_path);
@@ -369,73 +377,63 @@ int dir_tree_walk_for_files(const char *dir_path)
 #endif
 }
 
-/* make delta patching a lot easier */
-void swap_data(struct file_info *a, struct file_info *b)
+/* do sorting */
+void front_back_split(struct file_entry *src, struct file_entry **front_ref, struct file_entry **back_ref)
 {
-	char *path = a->path;
-	uint64_t modified_time = a->modified_time;
-	uint64_t size = a->size;
-	int type = a->type;
+	struct file_entry *fast, *slow;
 
-	a->path = b->path;
-	a->modified_time = b->modified_time;
-	a->size = b->size;
-	a->type = b->type;
+	slow = src;
+	fast = src->next;
 
-	b->path = path;
-	b->modified_time = modified_time;
-	b->size = size;
-	b->type = type;
-}
-
-#if 0
-
-#define m_max(X, Y) (((X) > (Y)) ? (X) : (Y))
-
-int sorter(const char *a, const char *b)
-{
-	size_t a_len = strlen(a);
-	size_t b_len = strlen(b);
-	size_t l = m_max(a_len, b_len);
-	size_t i;
-
-	for (i = 0; i < l; i++) {
-		if (a[i] > b[i]) return 1;
-		if (a[i] < b[i]) return -1;
-		if (a_len > b_len) return 1;
-		if (a_len < b_len) return -1;
+	while (fast != NULL) {
+		fast = fast->next;
+		if (fast != NULL) {
+			slow = slow->next;
+			fast = fast->next;
+		}
 	}
 
-	return 0;
+	(*front_ref) = src;
+	(*back_ref) = slow->next;
+	slow->next = NULL;
 }
 
-#endif
-
-void sort_dir_tree()
+struct file_entry *sorted_merge_paths(struct file_entry *a, struct file_entry *b)
 {
-	struct file_info *fi_0, *fi_1;
-	int swapped;
+	struct file_entry *res = NULL;
+	int cmp;
 
-	fi_1 = NULL;
+	if (a == NULL)
+		return b;
+	else if (b == NULL)
+		return a;
 
-	do {
-		swapped = 0;
-		fi_0 = fi_head;
+	cmp = strcmp(a->entry.path, b->entry.path);
 
-		while (fi_0->next != fi_1) {
-			int wh = strcmp(fi_0->path, fi_0->next->path);
-			if (wh > 0) {
-				swap_data(fi_0, fi_0->next);
-				swapped = 1;
-			} else if (wh < 0) {
-				swap_data(fi_0->next, fi_0);
-				swapped = 1;
-			}
-			fi_0 = fi_0->next;
-		}
+	if (cmp <= 0) {
+		res = a;
+		res->next = sorted_merge_paths(a->next, b);
+	} else {
+		res = b;
+		res->next = sorted_merge_paths(a, b->next);
+	}
 
-		fi_1 = fi_0;
-	} while (swapped);
+	return res;
+}
+
+void merge_sort_paths(struct file_entry **head_ref)
+{
+	struct file_entry *head = (*head_ref), *a, *b;
+
+	if (head == NULL || head->next == NULL)
+		return;
+
+	front_back_split(head, &a, &b);
+
+	merge_sort_paths(&a);
+	merge_sort_paths(&b);
+
+	(*head_ref) = sorted_merge_paths(a, b);
 }
 
 void pakar_validate_index(FILE *fp)
@@ -453,7 +451,7 @@ void pakar_validate_all(FILE *index_fp)
 	(void)index_fp;
 }
 
-// why? because windows.
+/* why? because windows. */
 char *work_strdup(const char *src)
 {
 	char *s = malloc(strlen(src) + 1);
@@ -469,10 +467,14 @@ char *work_strdup(const char *src)
 char *work_basename(const char *path)
 {
 	char *s = strrchr(path, '/');
-	if (s == NULL)
+	if (s == NULL) {
+		if ((s = strrchr(path, '\\')) != NULL)
+			return work_strdup(s + 1);
+
 		return work_strdup(path);
-	else
+	} else {
 		return work_strdup(s + 1);
+	}
 }
 
 int work_in_console()
@@ -547,6 +549,7 @@ FILE *fopen_index(const char *name, const char *open_mode)
 		}
 
 		strcpy(path, tmp_s);
+
 		free(tmp_s);
 	} else {
 		path = malloc(strlen(name) + 11);
@@ -574,18 +577,53 @@ int pakar_op_create(const char *path, int argc, char **argv)
 	struct serialize_stream stream;
 	char stream_buf[512], holder[4096];
 	FILE *index_fp, *chunk_fp, *entry_fp;
-	struct file_info *fi;
+	struct file_entry *fi;
 	uint64_t offset;
 	int retcode;
-
-	struct file_entry *fe_head, *fe_tmp;
 
 	(void)argc;
 	(void)argv;
 	(void)stream_buf;
 	(void)stream;
 
-	fe_head = NULL;
+	greater_path = path;
+	greater_path_len = strlen(path);
+
+#ifdef _WIN32
+	/* the search function goes '*.*' on windows. */
+	{
+		WCHAR *wstr;
+		uint64_t ms;
+		HANDLE handle;
+		WIN32_FIND_DATAW find_data;
+
+		if (conv_utf8_to_wchr(&wstr, path) != 0) {
+			oom();
+			return 2;
+		}
+
+		handle = FindFirstFileW(wstr, &find_data);
+		free(wstr);
+
+		/* the user might be evil sometimes */
+		if (handle == INVALID_HANDLE_VALUE) {
+dir_not_found:
+			fprintf(stderr, "directory not found: %s\n", path);
+			return 2;
+		}
+
+		if ((find_data.dwFileAttributes & FILE_ATTRIBUTE_DIRECTORY) == 0) {
+			CloseHandle(handle);
+			goto dir_not_found;
+		}
+
+		conv_filetime_to_u64_ms(&find_data.ftLastWriteTime, &ms);
+
+		file_info_add(path, ms, 0, FILE_INFO_DIR);
+
+		CloseHandle(handle);
+	}
+#endif
 
 	if (dir_tree_walk_for_files(path) != 0) {
 		fprintf(stderr, "error while doing directory walk in: %s!\n", path);
@@ -593,7 +631,7 @@ int pakar_op_create(const char *path, int argc, char **argv)
 		return 2;
 	}
 
-	sort_dir_tree();
+	merge_sort_paths(&fi_head);
 
 	index_header.begin = 0x3F212A7C;
 	index_header.version = PAKAR_VERSION;
@@ -601,50 +639,6 @@ int pakar_op_create(const char *path, int argc, char **argv)
 	index_header.num_chunks = 1;
 	index_header.size = fi_total_size;
 	memset(index_header.sha256, 0, sizeof(index_header.sha256));
-
-	/* regular processing */
-	for (fi = fi_head; fi != NULL; fi = fi->next) {
-		if (fi->type != FILE_INFO_REG)
-			continue;
-
-		fe_tmp = malloc(sizeof(struct file_entry));
-		fe_tmp->entry.crc32 = 0xFFFFFFFF;
-		fe_tmp->entry.modified_time = fi->modified_time;
-		fe_tmp->entry.size = fi->size;
-		fe_tmp->entry.offset = 0;
-		fe_tmp->entry.type = fi->type;
-		fe_tmp->path = fi->path;
-		memcpy((char *)fe_tmp->entry.path, fi->path + strlen(path), sizeof(fe_tmp->entry.path));
-
-		fe_tmp->next = fe_head;
-		fe_head = fe_tmp;
-	}
-
-	/* directory processing */
-	for (fi = fi_head; fi != NULL; fi = fi->next) {
-		if (fi->type != FILE_INFO_DIR)
-			continue;
-
-		fe_tmp = malloc(sizeof(struct file_entry));
-		fe_tmp->entry.crc32 = 0xDEADBEEF;
-		fe_tmp->entry.modified_time = fi->modified_time;
-		fe_tmp->entry.size = 0;
-		fe_tmp->entry.offset = 0xFFFFFFFFFFFFFFFF;
-		fe_tmp->entry.type = fi->type;
-
-		// hardcode root path for the pak
-		if (strcmp(fi->path, path) == 0) {
-			fe_tmp->path = NULL;
-			strcpy(fe_tmp->entry.path, "/");
-		} else {
-			fe_tmp->path = fi->path;
-			memcpy((char *)fe_tmp->entry.path, fi->path + strlen(path), sizeof(fe_tmp->entry.path));
-		}
-
-
-		fe_tmp->next = fe_head;
-		fe_head = fe_tmp;
-	}
 
 	if ((index_fp = fopen_index(path, "wb")) == NULL) {
 		retcode = 2;
@@ -654,8 +648,8 @@ int pakar_op_create(const char *path, int argc, char **argv)
 	serialize_stream_init(&stream, stream_buf, sizeof(stream_buf));
 
 	/* chunking */
-	for (retcode = 0, chunk_fp = NULL, fe_tmp = fe_head; fe_tmp != NULL; fe_tmp = fe_tmp->next) {
-		if (fe_tmp->entry.type == FILE_INFO_DIR)
+	for (retcode = 0, chunk_fp = NULL, fi = fi_head; fi != NULL; fi = fi->next) {
+		if (fi->entry.type == FILE_INFO_DIR)
 			continue;
 
 		if (chunk_fp == NULL) {
@@ -676,13 +670,13 @@ int pakar_op_create(const char *path, int argc, char **argv)
 			offset = 0;
 		}
 
-		if (fe_tmp->path == NULL)
+		if (fi->actual_path == NULL)
 			continue;
 
-		if ((entry_fp = fopen(fe_tmp->path, "rb")) == NULL)
+		if ((entry_fp = fopen(fi->actual_path, "rb")) == NULL)
 			continue;
 
-		fe_tmp->entry.offset = offset;
+		fi->entry.offset = offset;
 
 		do {
 			memset(holder, 0, sizeof(holder));
@@ -703,8 +697,8 @@ int pakar_op_create(const char *path, int argc, char **argv)
 	fwrite(stream.data, 1, stream.offset, index_fp);
 	serialize_stream_seek(&stream, 0, SERIALIZE_STREAM_SEEK_BEGIN);
 
-	for (fe_tmp = fe_head; fe_tmp != NULL; fe_tmp = fe_tmp->next) {
-		serialize_stream_write_pakar_index_entry(&stream, &fe_tmp->entry);
+	for (fi = fi_head; fi != NULL; fi = fi->next) {
+		serialize_stream_write_pakar_index_entry(&stream, &fi->entry);
 		fwrite(stream.data, 1, stream.offset, index_fp);
 		serialize_stream_seek(&stream, 0, SERIALIZE_STREAM_SEEK_BEGIN);
 	}
@@ -712,12 +706,6 @@ int pakar_op_create(const char *path, int argc, char **argv)
 	fclose(index_fp);
 cleanup:
 	file_info_cleanup();
-
-	while (fe_head != NULL) {
-		fe_tmp = fe_head->next;
-		free(fe_head);
-		fe_head = fe_tmp;
-	}
 
 	return retcode;
 }
