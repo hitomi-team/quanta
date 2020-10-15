27762395b5a7fda95f54029fd075ef0beebe86da
koukuno <kuze.genkei@protonmail.com>
2020-10-15 13:35:40 -0700

    vulkan: add VMA

--- src/layer1/renderer/subsystems/vulkan/vulkanrequired.h
+++ src/layer1/renderer/subsystems/vulkan/vulkanrequired.h
@@ -2,6 +2,10 @@
 #define RENDERER_VULKANREQUIRED_H
 
 #include "volk.h"
+
+#define VMA_DYNAMIC_VULKAN_FUNCTIONS 0
+#include "vk_mem_alloc.h"
+
 #include <SDL2/SDL_vulkan.h>
 
 #define VK_ASSERT(X, STR) do { if (X != VK_SUCCESS) { global_log.Error(FMT_STRING("vk error {:08X}: {}"), X, STR); abort(); } } while (0);
