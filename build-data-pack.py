e1592e02f522cb7cd68777cf5102a9df451aba9c
koukuno <kuze.genkei@protonmail.com>
2021-01-07 21:05:23 -0800

    tools: build data.zip in source dir and add in .gitignore

--- build-data-pack.py
+++ build-data-pack.py
@@ -4,17 +4,23 @@
 
 import os
 import zipfile
+import argparse
+
+parser = argparse.ArgumentParser(description='build data pack')
+parser.add_argument('-o', type=str, help='output directory')
+args = parser.parse_args()
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
 
-try:
-    os.mkdir(os.path.join(dir_path, "build"))
-except:
-    print("build dir already exists...")
+open_path = os.path.dirname(os.path.realpath(__file__))
+if (args.o != None):
+    open_path = args.o
 
 print("building data pack")
 
-z = zipfile.ZipFile(os.path.join(dir_path, "build/data.zip"), "w", compression=zipfile.ZIP_STORED) 
+z = zipfile.ZipFile(os.path.join(open_path, "data.zip"), "w", compression=zipfile.ZIP_STORED) 
+
+# this is done so the zip shows the inlined content of data
 os.chdir(os.path.join(dir_path, "data"))
 
 for root, dirs, files in os.walk("."):
